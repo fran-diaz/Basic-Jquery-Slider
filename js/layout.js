06ae9afc3dee3b8a559df6987bd7f52081c46f5a
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2015-01-23 11:44:32 +0100

    Example finished.

--- js/layout.js
+++ js/layout.js
@@ -1,43 +1,44 @@
+/**************************** SPLASH CONTROL *******************************/
 $(document).ready(function(){
-    splash_num = $("#splash li").length;
-    $("#splash").append("<div id='splash_nav'><ul></ul></div>");
+    splash_num = $("#splash > section").length;
+    $('#splash').parent().append("<div id='splash_nav'><ul></ul></div>");
 
     for (t = 0; t < splash_num; t++) {
         $("#splash_nav ul").append('<li></li>');
     }
-
     $("#splash_nav li").eq(0).addClass("active");
 
     current_splash = 0;
-    
+
     splash_interval = setInterval(function () {
-        $('#splash_main li.active').removeClass('active');
+        $('#splash > section.active').removeClass('active');
         $('#splash_nav li.active').removeClass('active');
 
-        if($(window).width() > 960) {$('#splash_main li').eq(current_splash).fadeOut(500);} 
-        else {$('#splash_main li').eq(current_splash).hide();}
+        if($(window).width() > 960) {$('#splash > section').eq(current_splash).fadeOut(500);} 
+        else {$('#splash > section').eq(current_splash).hide();}
 
         if (current_splash === splash_num - 1) {current_splash = 0;} 
         else {current_splash++;}
 
-        if($(window).width() > 960) {$('#splash_main li').eq(current_splash).fadeIn(1000);} 
-        else{$('#splash_main li').eq(current_splash).show();}
+        if($(window).width() > 960) {$('#splash > section').eq(current_splash).fadeIn(1000);} 
+        else{$('#splash > section').eq(current_splash).show();}
 
-        $('#splash_main li').eq(current_splash).addClass('active');
+        $('#splash > section').eq(current_splash).addClass('active');
         $('#splash_nav li').eq(current_splash).addClass('active');
 
-    }, 6000);
+    }, 3000);
 });
 
 $(document).on('click','#splash_nav li', function () {
-    $("#splash_main li.active").fadeOut();
-    $("#splash_main li.active").removeClass("active");
+    $("#splash > section.active").fadeOut();
+    $("#splash > section.active").removeClass("active");
 
     $("#splash_nav li.active").removeClass("active");
 
     $(this).addClass("active");
     splash_interval = window.clearInterval(splash_interval);
 
-    $("#splash_main li").eq($("#splash_nav li").index(this)).fadeIn();
-    $("#splash_main li").eq($("#splash_nav li").index(this)).addClass("active");
+    $("#splash > section").eq($("#splash_nav li").index(this)).fadeIn();
+    $("#splash > section").eq($("#splash_nav li").index(this)).addClass("active");
+
 });
